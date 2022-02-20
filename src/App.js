3addb9d9545fea1f42d8ee1d3ae50ad4c97b3974
Yongrok <94chl@naver.com>
2022-02-20 19:17:19 +0900

    refactor: App에 Canvas, Tool 컴포넌트 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./App.module.scss";
+import { Canvas, Tool } from "@/component";
+
+const { container } = styles;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className={classNames(container)}>
+      <Canvas></Canvas>
+      <Tool></Tool>
+    </div>
+  );
 }
 
 export default App;

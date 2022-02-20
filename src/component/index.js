2034f4515cda92536a4873109258b2363f00f795
Yongrok <94chl@naver.com>
2022-02-20 20:26:15 +0900

    deploy 브랜치 파일 정리

--- src/component/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Canvas } from "./Canvas/Canvas";
-export { default as Tool } from "./Tool/Tool";
-export { default as Button } from "./Button/Button";
-export { default as CustomInput } from "./CustomInput/CustomInput";
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { canvas } from "./canvas/canvas";
-
-export const store = configureStore({
-  reducer: {
-    canvas: canvas.reducer,
-  },
-});

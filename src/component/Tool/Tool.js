cbb9744bb159c067d8643c70dbce9e4993d2162d
Yongrok <94chl@naver.com>
2022-02-22 19:50:22 +0900

    refactor: 사용상 안내사항 추가

--- src/component/Tool/Tool.js
+++ src/component/Tool/Tool.js
@@ -8,7 +8,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { canvas } from "@/store/canvas/canvas";
 import { useLocalStorage } from "@/hook";
 
-const { container, shapeButtonBox, customInputBox, controlButtonBox } = styles;
+const {
+  container,
+  shapeButtonBox,
+  customInputBox,
+  controlButtonBox,
+  guideBox,
+} = styles;
 
 const Tool = () => {
   const dispatch = useDispatch();
@@ -48,7 +54,7 @@ const Tool = () => {
   );
   const [storedLayersNow, setStoredLayersNow] = useLocalStorage(
     "storedLayersNow",
-    []
+    -1
   );
 
   const setShapeButton = (type) => {
@@ -127,94 +133,114 @@ const Tool = () => {
   return (
     <div className={classNames(container)}>
       <div className={classNames(shapeButtonBox)}>
-        <Button
-          onClick={() => setShapeButton("line")}
-          isActive={shapeType === "line"}
-        >
-          직선
-        </Button>
-        <Button
-          onClick={() => setShapeButton("spline")}
-          isActive={shapeType === "spline"}
-        >
-          곡선
-        </Button>
-        <Button
-          onClick={() => setShapeButton("ellipse")}
-          isActive={shapeType === "ellipse"}
-        >
-          원
-        </Button>
-        <Button
-          onClick={() => setShapeButton("rect")}
-          isActive={shapeType === "rect"}
-        >
-          직사각형
-        </Button>
-        <Button
-          onClick={() => setShapeButton("polygon")}
-          isActive={shapeType === "polygon"}
-        >
-          다각형
-        </Button>
+        <h3>드로잉 타입</h3>
+        <div>
+          <Button
+            onClick={() => setShapeButton("line")}
+            isActive={shapeType === "line"}
+          >
+            직선
+          </Button>
+          <Button
+            onClick={() => setShapeButton("spline")}
+            isActive={shapeType === "spline"}
+          >
+            곡선
+          </Button>
+          <Button
+            onClick={() => setShapeButton("ellipse")}
+            isActive={shapeType === "ellipse"}
+          >
+            원
+          </Button>
+          <Button
+            onClick={() => setShapeButton("rect")}
+            isActive={shapeType === "rect"}
+          >
+            직사각형
+          </Button>
+          <Button
+            onClick={() => setShapeButton("polygon")}
+            isActive={shapeType === "polygon"}
+          >
+            다각형
+          </Button>
+        </div>
       </div>
       <div className={classNames(customInputBox)}>
-        <CustomInput
-          inputId="strokeColor"
-          onChange={(e) => onChangeColor(e, "strokeColor")}
-          value={strokeColor}
-          inputOption={{ type: "color" }}
-        >
-          테두리 색상
-        </CustomInput>
-      </div>
-      <div>
-        <CustomInput
-          inputId="strokeWidth"
-          onChange={(e) => onChangeStrokeWidth(e)}
-          value={strokeWidth}
-          inputOption={{ type: "number", min: 5, max: 50 }}
-        >
-          테두리 두께
-        </CustomInput>
-        <CustomInput
-          inputId="fillColor"
-          onChange={(e) => onChangeColor(e, "fillColor")}
-          value={fillColor === "transparent" ? tempFillColor : fillColor}
-          inputOption={{ type: "color" }}
-        >
-          채우기 색상
-        </CustomInput>
-        <CustomInput
-          inputId="fillColorTransparency"
-          onChange={(e) => onChangeFillColorOpacity(e)}
-          value={0}
-          inputOption={{ type: "checkbox", checked: fillColorTransparency }}
-        >
-          채우기 없음
-        </CustomInput>
+        <h3>드로잉 옵션</h3>
+        <div>
+          <CustomInput
+            inputId="strokeColor"
+            onChange={(e) => onChangeColor(e, "strokeColor")}
+            value={strokeColor}
+            inputOption={{ type: "color" }}
+          >
+            테두리 색상
+          </CustomInput>
+          <CustomInput
+            inputId="strokeWidth"
+            onChange={(e) => onChangeStrokeWidth(e)}
+            value={strokeWidth}
+            inputOption={{ type: "number", min: 5, max: 50 }}
+          >
+            테두리 두께
+          </CustomInput>
+          <CustomInput
+            inputId="fillColor"
+            onChange={(e) => onChangeColor(e, "fillColor")}
+            value={fillColor === "transparent" ? tempFillColor : fillColor}
+            inputOption={{ type: "color" }}
+          >
+            채우기 색상
+          </CustomInput>
+          <CustomInput
+            inputId="fillColorTransparency"
+            onChange={(e) => onChangeFillColorOpacity(e)}
+            value={0}
+            inputOption={{ type: "checkbox", checked: fillColorTransparency }}
+          >
+            채우기 없음
+          </CustomInput>
+        </div>
       </div>
       <div className={classNames(controlButtonBox)}>
-        <Button onClick={undo} isActive={false} isDisable={layersNow < 0}>
-          undo
-        </Button>
-        <Button
-          onClick={redo}
-          isActive={false}
-          isDisable={layersNow >= layersHistory.length - 1}
-        >
-          redo
-        </Button>
-        <Button
-          onClick={() => shapes.length && clearShapes()}
-          isActive={false}
-          isDisable={!shapes.length}
-        >
-          clear
-        </Button>
-        <Button onClick={resetLayers} isActive={false}>
-          reset
-        </Button>
+        <h3>편집</h3>
+        <div>
+          <Button onClick={undo} isActive={false} isDisable={layersNow < 0}>
+            undo
+          </Button>
+          <Button
+            onClick={redo}
+            isActive={false}
+            isDisable={layersNow >= layersHistory.length - 1}
+          >
+            redo
+          </Button>
+          <Button
+            onClick={() => shapes.length && clearShapes()}
+            isActive={false}
+            isDisable={!shapes.length}
+          >
+            clear
+          </Button>
+          <Button onClick={resetLayers} isActive={false}>
+            reset
+          </Button>
+        </div>
+      </div>
+      <div className={classNames(guideBox)}>
+        <h3>사용 정보</h3>
+        <ul>
+          <li>
+            <span>esc:</span>
+            <span>진행 중인 작업 취소</span>
+          </li>
+          <li>
+            <span>더블클릭:</span>
+            <span>진행 중인 작업(곡선, 다각형)을 해당 위치에서 완료</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
